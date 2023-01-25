aa67a647932e5943795b69f2ff0c2589612f4ade
Dmitrii Vartik <dekesite@mail.ru>
2023-01-25 23:02:18 +0300

    first commit

---
+++ src/test/java/guru/qa/SelenideTest.java
@@ -0,0 +1,34 @@
+package guru.qa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTest {
+
+    @BeforeAll
+    static void configure() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @DisplayName("Проверка наличия SoftAssertions в wiki selenide")
+    @Test
+    void githubTest() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("button.js-wiki-more-pages-link").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("#wiki-content").shouldHave(text("Using JUnit5 extend test class"));
+        $("#wiki-body").$(withText("Using JUnit5 extend test class")).shouldBe(visible);
+    }
+
+}
