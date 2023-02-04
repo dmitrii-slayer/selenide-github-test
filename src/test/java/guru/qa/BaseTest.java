33a7e02d7e00144b21908df151915301bafc35b2
Dmitrii Vartik <dekesite@mail.ru>
2023-02-04 14:53:32 +0300

    added hover and drag n drop tests

--- src/test/java/guru/qa/BaseTest.java
+++ src/test/java/guru/qa/BaseTest.java
@@ -1,6 +1,8 @@
 package guru.qa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class BaseTest {
@@ -8,6 +10,7 @@ public class BaseTest {
     @BeforeAll
     static void configure() {
         Configuration.browserSize = "1920x1080";
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
 }
--- src/test/java/guru/qa/SelenideTest.java
+++ src/test/java/guru/qa/SelenideTest.java
@@ -1,26 +1,97 @@
 package guru.qa;
 
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class SelenideTest extends BaseTest {
 
     @DisplayName("Проверка наличия SoftAssertions в wiki selenide")
     @Test
-    void githubTest() {
-        open("https://github.com/selenide/selenide");
-        $("#wiki-tab").click();
-        $("button.js-wiki-more-pages-link").click();
-        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
-        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
-        $("#wiki-content").shouldHave(text("Using JUnit5 extend test class"));
-        $("#wiki-body").$(withText("Using JUnit5 extend test class")).shouldBe(visible);
+    void selenideSoftAssertionsTest() {
+        step("Открыть страницу Selenide в Github", () -> {
+            open("https://github.com/selenide/selenide");
+        });
+
+        step("Перейти в раздел Wiki", () -> {
+            $("#wiki-tab").click();
+        });
+
+        step("В боковом меню есть раздел SoftAssertions", () -> {
+            $("div[id = 'wiki-pages-box']").$(withText("more pages")).click();
+            $("div[id = 'wiki-pages-box']").$(byText("SoftAssertions")).shouldBe(visible).click();
+        });
+
+        step("На странице SoftAssertions есть пример кода для JUnit5", () -> {
+            $("#wiki-body").$(withText("Using JUnit5 extend test class")).shouldBe(visible);
+        });
+
     }
 
+    @DisplayName("Проверка команды hover()")
+    @Test
+    void enterpriseHoverTest() {
+        SelenideElement solutionsButton = $$(".HeaderMenu-item").findBy(text("Solutions"));
+
+        step("Открыть главную страницу Github", () -> {
+            open("https://github.com/");
+        });
+
+        step("В верхнем меню навести курсор на Solutions", () -> {
+            solutionsButton.hover();
+        });
+
+        step("Кликнуть по разделу Enterprise из открывшегося списка", () -> {
+            solutionsButton.parent().$(byText("Enterprise")).click();
+        });
+
+        step("На открытой странице должен быть заголовок с текстом Build like the best", () -> {
+            $(byTagAndText("h1", "Build like the best")).shouldBe(visible);
+        });
+    }
+
+//    @Test
+//    @DisplayName("Drag-n-drop test using Actions")
+//    public void dragAndDropActionsTest() {
+//
+//        SelenideElement rectA = $("#column-a"),
+//                rectB = $("#column-b");
+//
+//        step("Открыть страницу drag-n-drop", () -> {
+//            open("https://the-internet.herokuapp.com/drag_and_drop");
+//        });
+//
+//        step("Перенести прямоугольник А на место В", () -> {
+//            actions().dragAndDrop(rectA, rectB).perform();
+//        });
+//
+//        step("Текст первого прямоугольника должен быть В", () -> {
+//            $("#columns div").shouldHave(text("B"));
+//        });
+//    }
+
+    @Test
+    @DisplayName("Drag-n-drop test using Selenide")
+    public void dragAndDropSimpleTest() {
+
+        SelenideElement rectA = $("#column-a"),
+                rectB = $("#column-b");
+
+        step("Открыть страницу drag-n-drop", () -> {
+            open("https://the-internet.herokuapp.com/drag_and_drop");
+        });
+
+        step("Перенести прямоугольник А на место В", () -> {
+            rectA.dragAndDropTo(rectB);
+        });
+
+        step("Текст первого прямоугольника должен быть В", () -> {
+            $("#columns div").shouldHave(text("B"));
+        });
+    }
 }
